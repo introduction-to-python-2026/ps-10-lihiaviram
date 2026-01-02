7a126675bbf8090e29196e9389c36172c650e820
lihiaviram <lihiviram@gmail.com>
2026-01-03 00:36:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ my_image = load_image('dogphoto.jpg')
 image_gray_temp = np.mean(my_image, axis=2)
 image_cleaned = median(image_gray_temp, ball(3))
 final_edges = edge_detection(my_image)
-threshold_value = 80
+threshold_value = 50
 edge_binary = final_edges > threshold_value
 binary_image_data = edge_binary.astype(np.uint8) * 255
 edge_image = Image.fromarray(binary_image_data )
