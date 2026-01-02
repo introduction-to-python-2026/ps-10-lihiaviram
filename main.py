cba26cfbaaed498b678f629840f1b8b002ac6dda
lihiaviram <lihiviram@gmail.com>
2026-01-03 00:33:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from PIL import Image
 from image_utils import load_image, edge_detection
 import numpy as np
+from skimage.filters import median
+from skimage.morphology import ball
 
 my_image = load_image('dogphoto.jpg')
+image_gray_temp = np.mean(my_image, axis=2)
+image_cleaned = median(image_gray_temp, ball(3))
 final_edges = edge_detection(my_image)
 threshold_value = 80
 edge_binary = final_edges > threshold_value
