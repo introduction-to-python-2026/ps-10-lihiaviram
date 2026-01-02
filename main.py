a9a949daef0cfbc5d60353031b2aeb0cd2123c9a
lihiaviram <lihiviram@gmail.com>
2026-01-02 23:54:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from skimage.morphology import ball
 my_image = load_image('dogphoto.jpg')
 clean_image = median(my_image, ball(3))
 final_edges = edge_detection(my_image)
-threshold_value = 50
+threshold_value = 60
 edge_binary = final_edges > threshold_value
 binary_image_data = edge_binary.astype(np.uint8) * 255
 edge_image = Image.fromarray(binary_image_data )
